c7e495817bbd49a644c57f03c3e76335cdedef7e
Gabriel "Kinash <gabriel@kinash.xyz>
2022-04-03 15:41:01 -0300

    iswhitelisted function

--- contracts/FantomZombies.sol
+++ contracts/FantomZombies.sol
@@ -98,6 +98,10 @@ contract FantomZombies is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Paus
         }
     }
 
+    function isWhitelisted(address whitelistedAddress) public view onlyOwner returns (bool) {
+        return IX32PE(_virusContract).balanceOf(whitelistedAddress) > 0;
+    }
+
     function burn(uint256 tokenId) public {
         require(ownerOf(tokenId) == msg.sender, "You're not the owner");
         _burn(tokenId);
